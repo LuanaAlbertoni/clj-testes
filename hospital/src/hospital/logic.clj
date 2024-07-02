c5bc6d22c83bc1149754e22a65f3d5881ce356dd
LuanaAlbertoni <luanamalbertoni@gmail.com>
2024-07-02 12:36:47 -0300

    corrigindo aula 2

--- hospital/src/hospital/logic.clj
+++ hospital/src/hospital/logic.clj
@@ -1,5 +1,8 @@
 (ns hospital.logic)
 
+; Test Driven Development
+; Test Driven Design
+
 ; existe um problema de condicional quando o departamento não existe
 ;(defn cabe-na-fila?
 ;  [hospital departamento]
@@ -17,22 +20,39 @@
 ;        count
 ;        (< 5))))
 
-
+; também funciona mas usa o some->
 ; desvantagem/vantagem "menos explicito"
+; desvantagem qq um que der nil, devolve nil
 (defn cabe-na-fila?
   [hospital departamento]
-  ;(count(get hospital departamento))
-  ;some-> vai 'threadeando at'e encontrar um nulo
   (some-> hospital
-      departamento
-      count
-      (< 5)))
+          departamento
+          count
+          (< 5)))
 
+;(defn chega-em
+;  [hospital departamento pessoa]
+;  (if (cabe-na-fila? hospital departamento)
+;    (update hospital departamento conj pessoa)
+;    (throw (ex-info "Não cabe ninguém neste departamento" {:paciente pessoa}))))
+
+
+;(defn chega-em
+;  [hospital departamento pessoa]
+;  (if (cabe-na-fila? hospital departamento)
+;    (update hospital departamento conj pessoa)
+;    (throw (IllegalStateException. "Não cabe ninguém neste departamento." ))))
+
+
+; nil???
+;(defn chega-em
+;  [hospital departamento pessoa]
+;  (if (cabe-na-fila? hospital departamento)
+;    (update hospital departamento conj pessoa)))
 
 
 (defn chega-em
   [hospital departamento pessoa]
-  (if (cabe-na-fila? hospital departamneto)
+  (if (cabe-na-fila? hospital departamento)
     (update hospital departamento conj pessoa)
-    (throw (IllegalStateException. "Não cabe ninguém neste departamento.")))
-  )
+    (throw (IllegalStateException. "Não cabe ninguém neste departamento." ))))
--- hospital/test/hospital/logic_test.clj
+++ hospital/test/hospital/logic_test.clj
@@ -1,55 +1,59 @@
 (ns hospital.logic-test
-  (:require [clojure.test :refer :all])
-  (:require [hospital.logic :refer :all]))
-
+  (:require [clojure.test :refer :all]
+            [hospital.logic :refer :all]))
 
 (deftest cabe-na-fila?-test
 
   ; boundary tests
-  ; exatamente na borda e one off.
+  ; exatamente na borda e one off. -1, +1. <=, >=, =.
 
-  ;borda do zero
-  (testing "Que cabe na fila vazia"
+  ; checklist na minha cabeca. poderia ser um checklist no papel
+
+  ; borda do zero
+  (testing "Que cabe numa fila vazia"
     (is (cabe-na-fila? {:espera []}, :espera)))
 
-  ;borda do limite
-  (testing "Que nao cabe quando a fila esta cheia"
-    (is (not (cabe-na-fila? {:espera [1 2 3 4 5]}, :espera))))
+  ; borda do limite
+  (testing "Que não cabe na fila quando a fila está cheia"
 
+    ; é de simples leitura pois é sequencial
+    ; mas a desvantagem é que podemos errar em fazer coisas sequenciais
+    ;(is (not (cabe-na-fila? {:espera [1 2 3 4 5]}, :espera)))
 
-  ;one off da borda do limite para cima
-  (testing "Que nao cabe quando tem mais que uma fila cheia"
+    ; não precisa ser sequencial e no mundo real não é
+    ; portanto faça testes que não são sequenciais!
+    (is (not (cabe-na-fila? {:espera [1 5 37 54 21]}, :espera)))
+    )
+
+  ; one off da borda do limite pra cima
+  (testing "Que não cabe na fila quando tem mais do que uma fila cheia"
     (is (not (cabe-na-fila? {:espera [1 2 3 4 5 6]}, :espera))))
 
-
-  ; (testing "Que cabe quando tem pouco menos que uma fila cheia"
-  ; (is (not (cabe-na-fila? {:espera [1 2 3 4]}, :espera))))
-
-  ; (testing "Que cabe quando tem pouca fila"
-  ; (is (not (cabe-na-fila? {:espera [1 2]}, :espera))))
-
-
-  ;Os dois testes comentados poder ser unidos em apenas um testing:
-
-  (testing "Que cabe quando a fila nao esta cheia"
+  ; dentro das bordas
+  (testing "Que cabe na fila quando tem gente mas não está cheia"
     (is (cabe-na-fila? {:espera [1 2 3 4]}, :espera))
     (is (cabe-na-fila? {:espera [1 2]}, :espera)))
 
-
-  (testing "Que ... quando o departamento nao existe"
-    (is (not (cabe-na-fila? {:espera [1 2 3 4]}, :-raio-x))))
-  )
-
+  (testing "Que não cabe quando o departamento não existe"
+    (is (not (cabe-na-fila? {:espera [1 2 3 4]}, :raio-x)))))
 
 (deftest chega-em-test
-  (testing "Aceita pessoas enquanto cabem pessoas na fila"
+
+  (testing "aceita pessoas enquanto cabem pessoas na fila"
+
+    ; implementação ruim pois testa que escrevemos o que escrevemos.
+    ; isto é, testa que erramos o que erramos. e que acertamos o que acertamos.
+    ;(is (= (update {:espera [1, 2]} :espera conj 5)
+    ;       (chega-em {:espera [1, 2]}, :espera, 5)))
+
     (is (= {:espera [1, 2, 3, 4, 5]}
            (chega-em {:espera [1, 2, 3, 4]}, :espera, 5)))
 
+    ; teste não sequencial
     (is (= {:espera [1, 2, 5]}
            (chega-em {:espera [1, 2]}, :espera, 5))))
 
-  (testing "Nao aceita quando nao cabe na fila"
+  (testing "não aceita quando não cabe na fila"
     ; verificando que uma exception foi jogada
     ; código clássico horrível. usamos uma exception GENERICA.
     ; mas qq outro erro generico vai jogar essa exception, e nós vamos achar que deu certo
@@ -66,4 +70,3 @@
     ;(is (nil? (chega-em {:espera [1 35 42 64 21]}, :espera 76)))
     )
   )
-
